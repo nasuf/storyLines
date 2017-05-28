a5de1983feb2e9194b9441355a0078a7223d6e34
pocketContributor <shanian713@gmail.com>
2017-05-28 11:16:07 +0800

    update phase and story fields

--- storyLines/src/main/java/com/story/controller/StoryController.java
+++ storyLines/src/main/java/com/story/controller/StoryController.java
@@ -40,30 +40,36 @@ public class StoryController {
 	@RequestMapping(value = "/story", method = RequestMethod.POST)
 	public ResponseEntity<Map<String, Object>> addPhase(@RequestBody Phase phaseObject, 
 			@RequestParam("isNewStory") Boolean isNewStory,
-			@RequestParam(value = "storyId", required = false) String storyId) {
+			@RequestParam(value = "storyId", required = false) String storyId,
+			@RequestParam(value = "parentPhaseId", required = false) String parentPhaseId) {
 		if (isNewStory.equals(true)) {
 			Story story = new Story();
 			story.setDepth(Constant.ONE);
 			story.setPhaseCount(Constant.ONE);
 			story.setCreatedDate(new Date().getTime());
+			story.setLastUpdatedDate(new Date().getTime());
 			Story savedStory = this.storyRepository.save(story);
 			if (null == savedStory) {
 				return new ResponseEntity<Map<String, Object>>(
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Story [" + story + "] saved failed.").build(),
 						HttpStatus.BAD_REQUEST);
 			}
+			logger.debug("Save new story: [" + savedStory + "] successfully.");
 			phaseObject.setIsStart(true);
 			phaseObject.setLevel(Constant.ONE);
 			phaseObject.setStoryId(savedStory.getId());
 			phaseObject.setLike(Constant.ZERO);
 			phaseObject.setDislike(Constant.ZERO);
 			phaseObject.setCreatedDate(new Date().getTime());
+			phaseObject.setIsStart(true);
+			phaseObject.setIsEnd(true);
 			Phase savedPhase = this.phaseRepository.save(phaseObject);
 			if (null == savedPhase) {
 				return new ResponseEntity<Map<String, Object>>(
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Phase [" + phaseObject + "] saved failed.").build(),
 						HttpStatus.BAD_REQUEST);
 			}
+			logger.debug("Save new phase: [" + savedPhase + "] successfully.");
 			ArrayList<String> phaseIdList = new ArrayList<String>();
 			phaseIdList.add(savedPhase.getId());
 			savedStory.setPhaseIdList(phaseIdList);
@@ -73,6 +79,7 @@ public class StoryController {
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Story [" + savedStory + "] updated failed.").build(),
 						HttpStatus.BAD_REQUEST);
 			}
+			logger.debug("Add phase [" + savedPhase.getId() + "] to story: [" + updatedStory.getId() + "] successfully.");
 			Map<String, Object> data = new HashMap<String, Object>();
 			data.put(Constant.RESULT_STORY, updatedStory);
 			data.put(Constant.RESULT_PHASE, savedPhase);
@@ -84,18 +91,45 @@ public class StoryController {
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "StoryId can't be null").build(),
 						HttpStatus.BAD_REQUEST);
 			}
+			phaseObject.setStoryId(storyId);
+			phaseObject.setCreatedDate(new Date().getTime());
+			phaseObject.setIsStart(false);
+			phaseObject.setParentPhaseId(parentPhaseId);
+			Phase foundParentPhase = this.phaseRepository.findOne(parentPhaseId);
+			if (null == foundParentPhase) {
+				return new ResponseEntity<Map<String, Object>>(
+						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Can't find parent phase for: [" + parentPhaseId + "]").build(),
+						HttpStatus.BAD_REQUEST);
+			}
+			if (foundParentPhase.getIsEnd().equals(true)) {
+				phaseObject.setIsEnd(true);
+				// update parent phase isEnd to false
+				foundParentPhase.setIsEnd(false);
+				Phase updatedParentPhase = this.phaseRepository.save(foundParentPhase);
+				if (null == updatedParentPhase) {
+					return new ResponseEntity<Map<String, Object>>(
+							new HttpResult(Constant.RESULT_STATUS_FAILURE, "Parent phase [" + foundParentPhase + "] updated failed.").build(),
+							HttpStatus.BAD_REQUEST);
+				}
+			} else {
+				phaseObject.setIsEnd(false);
+			}
+			Phase savedPhase = this.phaseRepository.save(phaseObject);
+			if (null == savedPhase) {
+				return new ResponseEntity<Map<String, Object>>(
+						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Phase [" + phaseObject + "] saved failed.").build(),
+						HttpStatus.BAD_REQUEST);
+			}
+			logger.debug("Save new phase: [" + savedPhase + "] successfully.");
+			
 			Story foundStory = this.storyRepository.findOne(storyId);
 			if (null == foundStory) {
 				return new ResponseEntity<Map<String, Object>>(
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Can't find story").build(),
 						HttpStatus.BAD_REQUEST);
 			}
-			phaseObject.setCreatedDate(new Date().getTime());
-			Phase savedPhase = this.phaseRepository.save(phaseObject);
-			if (null == savedPhase) {
-				return new ResponseEntity<Map<String, Object>>(
-						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Phase [" + phaseObject + "] saved failed.").build(),
-						HttpStatus.BAD_REQUEST);
+			if (savedPhase.getIsEnd().equals(true)) {
+				foundStory.setDepth(foundStory.getDepth() + Constant.ONE);
 			}
 			foundStory.setPhaseCount(foundStory.getPhaseCount() + Constant.ONE);
 			ArrayList<String> phaseIdList = foundStory.getPhaseIdList();
@@ -108,6 +142,7 @@ public class StoryController {
 						new HttpResult(Constant.RESULT_STATUS_FAILURE, "Story [" + foundStory + "] saved failed.").build(),
 						HttpStatus.BAD_REQUEST);
 			}
+			logger.debug("Add phase [" + savedPhase.getId() + "] to story: [" + savedStory.getId() + "] successfully.");
 			Map<String, Object> data = new HashMap<String, Object>();
 			data.put(Constant.RESULT_STORY, savedStory);
 			data.put(Constant.RESULT_PHASE, savedPhase);
@@ -123,6 +158,7 @@ public class StoryController {
 					new HttpResult(Constant.RESULT_STATUS_FAILURE, "phaseId can't be null").build(),
 					HttpStatus.BAD_REQUEST);
 		}
+		logger.debug("phaseId: [" + phaseId + "], like: [" + like + "]");
 		Phase foundPhase = this.phaseRepository.findOne(phaseId);
 		if (null == foundPhase) {
 			return new ResponseEntity<Map<String, Object>>(
@@ -140,6 +176,7 @@ public class StoryController {
 					new HttpResult(Constant.RESULT_STATUS_FAILURE, "Phase [" + foundPhase + "] saved failed.").build(),
 					HttpStatus.BAD_REQUEST);
 		}
+		logger.debug("Update phase: [" + savedPhase.getId() + "] successfully");
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put(Constant.RESULT_PHASE, savedPhase);
 		return new ResponseEntity<Map<String, Object>>(new HttpResult(Constant.RESULT_STATUS_SUCCESS,
