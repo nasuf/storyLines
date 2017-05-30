25dba4f80fc2e08dfd7c67faf587b43aa29568cb
pocketContributor <shanian713@gmail.com>
2017-05-30 14:53:50 +0800

    validation

--- storyLines/src/main/resources/static/js/story.js
+++ storyLines/src/main/resources/static/js/story.js
@@ -1,11 +1,11 @@
 var storyListTab = Vue.component('story-list', {
 	
-	template: ' <div style="margin-top:20px" class="ui list"> \
+	template: ' <div style="margin-top:20px; margin-bottom:20px" class="ui list"> \
 					<div class="ui top attached tabular menu transparent"> \
 						<div class="right menu"> \
 							 <div class="item"> \
 							    <div class="ui transparent icon input"> \
-									<span class="right floated star" @click="routeTo(\'newStoryTab\')"><i class="write icon"></i> 开启新故事线 </span> \
+									<span class="right floated star" @click="routeTo(\'newStoryTab\')"><i class="write icon"></i> 创建新故事线 </span> \
 							    </div> \
 							 </div> \
 						</div> \
@@ -115,11 +115,61 @@ var newStoryTab = Vue.component('new-story',{
 						  <div class="field"> \
 						    <label>正文</label> \
 						    <textarea v-model="phase.content"></textarea> \
-						  </div> \
-						  <div class="ui submit button" @click="createStoryLine()">创建</div> \
-						  <div class="ui submit button" @click="routeTo(\'/\')">取消</div> \
-					</form> \
-				</div> \
+						  </div>\
+						  <div class="ui submit button" @click="alertModal(\'create\')">创建</div> \
+						  <div class="ui submit button" @click="alertModal(\'cancel\')">取消</div><br/> \
+					</form><br> \
+					<div id="alert-title" class="ui bottom floating hidden negative message"> <i class="close icon"></i>\
+						故事标题尚未填写 \
+				  	</div> \
+					<div id="alert-author" class="ui bottom floating hidden negative message"> <i class="close icon"></i>\
+						作者尚未填写 \
+				  	</div> \
+					<div id="alert-content" class="ui bottom floating hidden negative message"> <i class="close icon"></i>\
+						内容尚未填写 \
+				  	</div> \
+					<div class="ui basic modal create"> \
+					  <div class="header"> \
+						确定创建新故事线吗？ \
+					  </div> \
+					  <div class="image content"> \
+					    <div class="description"> \
+					    </div> \
+					  </div> \
+					  <div class="actions"> \
+					    <div class="two fluid ui inverted buttons"> \
+					      <div class="ui cancel red basic inverted button"> \
+					        <i class="remove icon"></i> \
+					        No \
+					      </div> \
+					      <div class="ui ok green basic inverted button" @click="createStoryLine()"> \
+					        <i class="checkmark icon"></i> \
+					        Yes \
+					      </div> \
+					    </div> \
+					  </div> \
+					</div> \
+					<div class="ui basic modal cancel"> \
+					  <div class="header"> \
+						确定取消吗？ \
+					  </div> \
+					  <div class="image content"> \
+					    <div class="description"> \
+					    </div> \
+					  </div> \
+					  <div class="actions"> \
+					    <div class="two fluid ui inverted buttons"> \
+					      <div class="ui cancel red basic inverted button"> \
+					        <i class="remove icon"></i> \
+					        No \
+					      </div> \
+					      <div class="ui ok green basic inverted button" @click="routeTo(\'/\')"> \
+					        <i class="checkmark icon"></i> \
+					        Yes \
+					      </div> \
+					    </div> \
+					  </div> \
+					</div> \
 		       </div>',
 	
     data: function() {
@@ -133,11 +183,46 @@ var newStoryTab = Vue.component('new-story',{
 				level: 1,
 				like: 0,
 				dislike: 0
-			}
+			},
+			isTitleComplete: false,
+			isAuthorComplete: false,
+			isContentComplete: false
 		}
 	},
 	
 	methods: {
+		
+		alertModal: function(action) {
+			if (action == 'create') {
+				debugger;
+				if (!this.phase.storyTitle) {
+					$('#alert-title').css('display','block');
+					return;
+				} else {
+					$('#alert-title').css('display','none');
+				}
+				if (!this.phase.author) {
+					$('#alert-author').css('display','block');
+					return;
+				} else {
+					$('#alert-author').css('display','none');
+				}
+				if (!this.phase.content) {
+					$('#alert-content').css('display','block');
+					return;
+				} else {
+					$('#alert-content').css('display','none');
+				}
+				$('.ui.basic.modal.create')
+				  .modal('show')
+				;
+			} else {
+				$('.ui.basic.modal.cancel')
+				  .modal('show')
+				;
+			}
+			
+		},
 		createStoryLine: function() {
 			var url = "/story/story?isNewStory=true";
 			var _self = this;
